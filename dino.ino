c7d74f0371894234668089e12d8086573276c718
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-08 11:53:36 +0800

    Update dino.ino

--- dino.ino
+++ dino.ino
@@ -13,20 +13,16 @@ const bool DINO[10][10]={
   {0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,1,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0}
 };
 bool jump,pressed;
 byte i,j,x,y,treeX,jumpMove;
 void dino(){
-  for(i=0;i<DINO_HEIGHT;i++){
-    for(j=0;j<DINO_WIDTH;j++){
-      display.drawPixel(x+j,y+i,(DINO[i][j]?SSD1306_WHITE:SSD1306_INVERSE));
-    }
-  }
+  for(i=0;i<DINO_HEIGHT;i++)for(j=0;j<DINO_WIDTH;j++)display.drawPixel(y+i,x+j,DINO[i][j]);
 }
 void setup(){
   Serial.begin(9600);
@@ -37,12 +33,12 @@ void setup(){
   display.clearDisplay();
 }
 void loop(){
-  display.fillRect(SCREEN_WIDTH-PLAY_WIDTH>>1,SCREEN_HEIGHT-PLAY_HEIGHT>>1,PALY_WIDTH,PLAY_HEIGHT,SSD1306_WHITE);
+  display.fillRect(SCREEN_WIDTH-PLAY_WIDTH>>1,SCREEN_HEIGHT-PLAY_HEIGHT>>1,PLAY_WIDTH,PLAY_HEIGHT,1);
   display.fillTriangle(
-    (SCREEN_WIDTH>>1)-(PALY_WIDTH>>2),(SCREEN_HEIGHT>>1)-(PLAY_HEIGHT>>2),
-    (SCREEN_WIDTH>>1)-(PALY_WIDTH>>2),SCREEN_HEIGHT>>1,
-    (SCREEN_WIDTH>>1)-(PALY_WIDTH>>2),(SCREEN_HEIGHT>>1)+(PLAY_HEIGHT>>2),
-    SSD1306_INVERSE
+    (SCREEN_WIDTH>>1)-(PLAY_WIDTH>>2),(SCREEN_HEIGHT>>1)-(PLAY_HEIGHT>>2),
+    (SCREEN_WIDTH>>1)+(PLAY_WIDTH>>2),SCREEN_HEIGHT>>1,
+    (SCREEN_WIDTH>>1)-(PLAY_WIDTH>>2),(SCREEN_HEIGHT>>1)+(PLAY_HEIGHT>>2),
+    0
   );
   display.display();
   jump=0;
@@ -50,23 +46,20 @@ void loop(){
   treeX=SCREEN_WIDTH-TREE_WIDTH;
   jumpMove=0;
   while(!digitalRead(BUTTON));
-  x=(SCREEN_WIDTH-PLAY_WIDTH)/+2;
-  y=(SCREEN_HEIGHT-PLAY_HEIGHT)/+2;
-  display.fillRect(x,y,PLAY_WIDTH,PLAY_HEIGHT,SSD1306_INVERSE);
-  x=0;
-  y=SCREEN_HEIGHT-DINO_HEIGHT;
+  display.fillRect(SCREEN_WIDTH-PLAY_WIDTH>>1,SCREEN_HEIGHT-PLAY_HEIGHT>>1,PLAY_WIDTH,PLAY_HEIGHT,0);
   dino();
   display.display();
+  while(1);
   while(jump||treeX>DINO_WIDTH){
-    if(treeX==SCREEN_WIDTH-TREE_WIDTH)display.fillRect(treeX,SCREEN_HEIGHT-TREE_HEIGHT,TREE_WIDTH,TREE_HEIGHT,SSD1306_INVERSE);
+    if(treeX==SCREEN_WIDTH-TREE_WIDTH)display.fillRect(treeX,SCREEN_HEIGHT-TREE_HEIGHT,TREE_WIDTH,TREE_HEIGHT,0);
     else{
-      display.fillRect(treeX,SCREEN_HEIGHT-TREE_HEIGHT,1,TREE_HEIGHT,SSD1306_WHITE);
-      display.fillRect(treeX+10,SCREEN_HEIGHT-TREE_HEIGHT,1,TREE_HEIGHT,SSD1306_INVERSE);
+      display.fillRect(treeX,SCREEN_HEIGHT-TREE_HEIGHT,1,TREE_HEIGHT,1);
+      display.fillRect(treeX+10,SCREEN_HEIGHT-TREE_HEIGHT,1,TREE_HEIGHT,0);
     }
     display.display();
     if(digitalRead(BUTTON)&&!jump&&!pressed){
       jump=pressed=1;
-      display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT,DINO_WIDTH,DINO_HEIGHT,SSD1306_INVERSE);
+      display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT,DINO_WIDTH,DINO_HEIGHT,0);
       y=SCREEN_HEIGHT-DINO_HEIGHT-TREE_HEIGHT;
       dino();
       display.display();
@@ -74,7 +67,7 @@ void loop(){
     if(jumpMove>=JUMP_MOVE){
       jump=0;
       jumpMove=0;
-      display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT-TREE_HEIGHT,DINO_WIDTH,DINO_HEIGHT,SSD1306_INVERSE);
+      display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT-TREE_HEIGHT,DINO_WIDTH,DINO_HEIGHT,0);
       y=SCREEN_HEIGHT-DINO_HEIGHT;
       dino();
       display.display();
