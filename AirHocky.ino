970eb90a61570b79c3d584296d97444800420f9f
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-11 17:12:23 +0800

    Update AirHocky.ino

--- AirHocky.ino
+++ AirHocky.ino
@@ -8,7 +8,8 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Ball Size
 const byte VRX1=A0,VRY1=A1,VRX2=A2,VRY2=A3,R=3;
-byte score1,score2,ballX,ballY,x1,y1,x2,y2,lx1,lx2,ly1,ly2,delX,delY;
+byte score1,score2,ballX,ballY,x1,y1,x2,y2,lx1,lx2,ly1,ly2;
+int delX,delY;
 void setup(){
   Serial.begin(9600);
   pinMode(VRX1,INPUT);
@@ -19,18 +20,18 @@ void setup(){
   digitalWrite(2,1);
   display.begin(SSD1306_SWITCHCAPVCC,0x3C);
   display.display();
-  //delay(1000);
+  delay(1000);
   display.clearDisplay();
-  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextSize(2);      // Normal 1:1 pixel scale
   display.setTextColor(1); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
 }
 void loop(){
   score1=score2=0;
   ballX=SCREEN_WIDTH>>1;
+  display.setCursor(0,0);
   display.print("move ahead");
   display.display();
-  //delay(1000);
+  delay(1000);
   display.clearDisplay();
   display.display();
   //while(analogRead(VRY1)<800|| analogRead(VRY2)<800);
@@ -66,20 +67,20 @@ void loop(){
         lx2=x2;
         ly2=y2;
       }
-      if((ballX-x1)<=(R<<1)&&abs(ballY-y1)<=(R<<1)){  // PlayeR 1 touch the ball
-        delX=(ballX-x1)/R>>1;
-        delY=(ballY-y1)/R>>1;
+      if((ballX-x1)*(ballX-x1)+(ballY-y1)*(ballY-y1)<=R*R){  // PlayeR 1 touch the ball
+        delX=(ballX-x1);
+        delY=(ballY-y1);
       }
-      if(abs(ballX-x2)<=(R<<1)&&abs(ballY-y2)<=(R<<1)){  // PlayeR 2 touch the ball
-        delX=(ballX-x2)/R>>1;
-        delY=(ballY-y2)/R>>1;
+      if((ballX-x2)*(ballX-x2)+(ballY-y2)*(ballY-y2)<=R*R){  // PlayeR 2 touch the ball
+        delX=(ballX-x2);
+        delY=(ballY-y2);
       }
       if(delX||delY){  // Ball move
         display.fillCircle(ballX,ballY,R,0);
         ballX+=delX;
         ballY+=delY;
         display.fillCircle(ballX,ballY,R,1);
-      display.display();
+        display.display();
       }
       if(ballX<R)delX=abs(delX);  // Ball touch the left, delX always positive
       if(ballX>SCREEN_WIDTH-R)delX=-abs(delX);  // Ball touch the Right, delX always negative
@@ -87,53 +88,38 @@ void loop(){
       if(ballY>SCREEN_HEIGHT-R)delY=-abs(delY);  // Ball touch the down, delY always negative
       display.display();
     }
-    byte side=80;
-    ballX=SCREEN_WIDTH>>1;
-    if(ballX<80){  // PlayeR 1 score
+    display.clearDisplay();
+    display.setCursor(0,0);
+    if(ballX>SCREEN_WIDTH>>1){  // PlayeR 1 score
       score1++;
-      ballX-=SCREEN_WIDTH>>3;
+      ballX=SCREEN_WIDTH>>1-SCREEN_WIDTH>>3;
+      display.print(1);
     }
     else{  // PlayeR 2 score
       score2++;
-      ballX+=SCREEN_WIDTH>>3;
-      side=0;
+      ballX=SCREEN_WIDTH>>1+SCREEN_WIDTH>>3;
+      display.print(2);
     }
-    // ScoRe animation
-    /*for(int i=0;i<2;i++){
-      //display.fillRect(side,25,80,80,1);
-      display.setCursor(30,60);
-      display.println(score2);
-      display.setCursor(30,60);
-      display.println(score1);
-      delay(500);
-      //display.fillRect(side,25,80,80,1);
-      area();
-      display.setCursor(30,60);
-      display.println(score2);
-      display.setCursor(30,60);
-      display.println(score1);
-      delay(500);
-    }*/
+    // Score animation
+    display.println(" scored");
+    display.print("1:");
+    display.print(score1);
+    display.println("/3");
+    display.print("2:");
+    display.print(score2);
+    display.println("/3");
+    display.display();
+    delay(1000);
   }
-
-  // Game end animation
-  display.fillScreen(BLACK);
-  byte winneR=2;
-  if(score1>2)winneR = 1;
-  display.setTextSize(2);
-  display.setCursor(10,40);
-  display.setTextColor(1);
-  display.print("playeR");
-  display.print(winneR);
-  display.print(" Win");
-  for (int i=1;i<26;i++){
-    display.fillRect(0, 105-i,160,i,1);
-  }
+  display.setCursor(0,0);
+  display.clearDisplay();
+  if(score1>2)display.print(1);
+  else display.print(2);
+  display.print(" won");
+  display.display();
   delay(2000);
 }
-
-// DRaw the area
-void area() {
+void area(){
   display.drawLine(0,20,0,44,1);    // Goal 1
   display.drawLine(127,20,127,44,1);  // Goal 2
   display.drawLine(64,0,64,29,1);   // CenteR line
