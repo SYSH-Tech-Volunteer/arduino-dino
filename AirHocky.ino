3860717d194025fe3ead098176e4cdca8d95756b
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-11 10:05:57 +0800

    Update AirHocky.ino

--- AirHocky.ino
+++ AirHocky.ino
@@ -24,7 +24,6 @@ void setup(){
   display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextColor(1); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);
 }
 void loop(){
   score1=score2=0;
@@ -46,51 +45,57 @@ void loop(){
     display.fillCircle(ballX,ballY,R,1);
     while(ballX>=R&&ballX<=SCREEN_WIDTH-R||ballY<=18||ballY>46){  // Ball in the area
       area();
+      display.display();
       x1=analogRead(VRX1)*64.0/1023;  // Locate Joystick 1
       y1=analogRead(VRY1)*64.0/1023;
       if(byte(lx1)!=byte(x1)||byte(ly1)!=byte(y1)){
         display.fillCircle(lx1,ly1,R,0);
         area();
         display.fillCircle(x1,y1,R,1);
+        display.display();
         lx1=x1;
         ly1=y1;
       }
-      x2=analogRead(VRX2)*32.0/1023+32;  // Locate Joystick 2
-      y2=analogRead(VRY2)*32.0/1023;
+      x2=analogRead(VRX2)*64.0/1023+64;  // Locate Joystick 2
+      y2=analogRead(VRY2)*64.0/1023;
       if(byte(lx2)!=byte(x2)||byte(ly2)!=byte(y2)){
         display.fillCircle(lx2,ly2,R,0);
         area();
         display.fillCircle(x2,y2,R,1);
+        display.display();
         lx2=x2;
         ly2=y2;
       }
-      if(abs(ballX-x1)<=(R<<1)&&abs(ballY-y1)<=(R<<1)){  // PlayeR 1 touch the ball
-        delX=(ballX-x1)/10;
-        delY=(ballY-y1)/10;
+      if((ballX-x1)<=(R<<1)&&abs(ballY-y1)<=(R<<1)){  // PlayeR 1 touch the ball
+        delX=(ballX-x1)/R>>1;
+        delY=(ballY-y1)/R>>1;
       }
       if(abs(ballX-x2)<=(R<<1)&&abs(ballY-y2)<=(R<<1)){  // PlayeR 2 touch the ball
-        delX=(ballX-x2)/10;
-        delY=(ballY-y2)/10;
+        delX=(ballX-x2)/R>>1;
+        delY=(ballY-y2)/R>>1;
       }
       if(delX||delY){  // Ball move
-        display.fillCircle(ballX,ballY,R,2);
+        display.fillCircle(ballX,ballY,R,0);
         ballX+=delX;
         ballY+=delY;
         display.fillCircle(ballX,ballY,R,1);
+      display.display();
       }
-      if(ballX<R)delX = abs(delX);  // Ball touch the left, delX always positive
-      if(ballX>160-R)delX=-abs(delX);  // Ball touch the Right, delX always negative
-      if(ballY<25+R)delY=abs(delY);  // Ball touch the up, delY always positive
-      if(ballY>105-R)delY=-abs(delY);  // Ball touch the down, delY always negative
+      if(ballX<R)delX=abs(delX);  // Ball touch the left, delX always positive
+      if(ballX>SCREEN_WIDTH-R)delX=-abs(delX);  // Ball touch the Right, delX always negative
+      if(ballY<R)delY=abs(delY);  // Ball touch the up, delY always positive
+      if(ballY>SCREEN_HEIGHT-R)delY=-abs(delY);  // Ball touch the down, delY always negative
+      display.display();
     }
     byte side=80;
+    ballX=SCREEN_WIDTH>>1;
     if(ballX<80){  // PlayeR 1 score
       score1++;
-      ballX=60;
+      ballX-=SCREEN_WIDTH>>3;
     }
     else{  // PlayeR 2 score
       score2++;
-      ballX=100;
+      ballX+=SCREEN_WIDTH>>3;
       side=0;
     }
     // ScoRe animation
@@ -121,7 +126,7 @@ void loop(){
   display.print("playeR");
   display.print(winneR);
   display.print(" Win");
-  for (int i=1;i<26;i++) {
+  for (int i=1;i<26;i++){
     display.fillRect(0, 105-i,160,i,1);
   }
   delay(2000);
@@ -134,5 +139,4 @@ void area() {
   display.drawLine(64,0,64,29,1);   // CenteR line
   display.drawLine(64,35,64,64,1);    // CenteR line
   display.drawCircle(64,32,R,1);     // CenteR ciRcle
-  display.display();
 }
