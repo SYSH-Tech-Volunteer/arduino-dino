39d323f33f3315e2ae0316ac7f4b10272da83fa9
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-11 22:12:57 +0800

    Update AirHocky.ino

--- AirHocky.ino
+++ AirHocky.ino
@@ -8,8 +8,8 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Ball Size
 const byte VRX1=A0,VRY1=A1,VRX2=A2,VRY2=A3,R=4;
-byte score1,score2,ballX,ballY,lastBallX,lastBallY,x1,y1,x2,y2,lx1,lx2,ly1,ly2;
-int delX,delY;
+byte score1,score2,ballX,ballY,x1,y1,x2,y2,lx1,lx2,ly1,ly2;
+int delX,delY,tmp;
 void setup(){
   pinMode(VRX1,INPUT);
   pinMode(VRY1,INPUT);
@@ -25,18 +25,18 @@ void setup(){
   display.display();//開機畫面
   delay(1000);
   display.clearDisplay();
-  display.setTextSize(2);      // Normal 1:1 pixel scale
-  display.setTextColor(1); // Draw white text
+  display.setTextSize(2);// Normal 1:1 pixel scale
+  display.setTextColor(1);// Draw blue text
 }
 void loop(){
   score1=score2=0;
-  ballX=SCREEN_WIDTH>>1;
+  ballX=SCREEN_WIDTH>>1;//球定位到中間
   display.clearDisplay();
   display.setCursor(0,0);
   display.print("move ahead");
   display.display();
-  while(analogRead(VRY1)<800|| analogRead(VRY2)<800);
-  while(score1<3&&score2<3){
+  while(analogRead(VRY1)<800|| analogRead(VRY2)<800);//等待直到兩個搖桿都向前
+  while(score1<3&&score2<3){//重複到有人3分
     delX=delY=0;
     x1=lx1=SCREEN_WIDTH>>2;
     x2=lx2=(SCREEN_WIDTH>>2)+(SCREEN_WIDTH>>1);
@@ -48,22 +48,22 @@ void loop(){
     display.fillCircle(x1,y1,R,1);
     display.fillCircle(x2,y2,R,1);
     display.fillCircle(ballX,ballY,R,1);
-    while(ballX>=R&&ballX<=SCREEN_WIDTH-R||ballY<=18||ballY>46){  // Ball in the area
+    while(ballX>=R&&ballX<=SCREEN_WIDTH-R||ballY<=18||ballY>46){//重複直到有人進洞
       area();
       display.display();
-      x1=analogRead(VRX1)*64.0/1023;  // Locate Joystick 1
+      x1=analogRead(VRX1)*64.0/1023;//計算第1位玩家座標
       y1=analogRead(VRY1)*64.0/1023;
-      if(byte(lx1)!=byte(x1)||byte(ly1)!=byte(y1)){
-        display.fillCircle(lx1,ly1,R,0);
+      if(lx1!=x1||ly1!=y1){//如果座標跟上一次不同
+        display.fillCircle(lx1,ly1,R,0);//擦掉
         area();
-        display.fillCircle(x1,y1,R,1);
+        display.fillCircle(x1,y1,R,1);//重畫
         display.display();
         lx1=x1;
         ly1=y1;
       }
-      x2=analogRead(VRX2)*64.0/1023+64;  // Locate Joystick 2
+      x2=analogRead(VRX2)*64.0/1023+64;//跟第1位玩家一樣
       y2=analogRead(VRY2)*64.0/1023;
-      if(byte(lx2)!=byte(x2)||byte(ly2)!=byte(y2)){
+      if(lx2!=x2||ly2!=y2){
         display.fillCircle(lx2,ly2,R,0);
         area();
         display.fillCircle(x2,y2,R,1);
@@ -72,17 +72,17 @@ void loop(){
         ly2=y2;
       }
       if((ballX-x1)*(ballX-x1)+(ballY-y1)*(ballY-y1)<=(R<<1)*(R<<1)){  // PlayeR 1 touch the ball
-        delX=(ballX-x1);
-        delY=(ballY-y1);
+        tmp=delX*(ballX-x1)+delY*(ballY-y1);
+        delX-=tmp*(ballX-x1)/R;
+        delY-=tmp*(ballY-y1)/R;
       }
       if((ballX-x2)*(ballX-x2)+(ballY-y2)*(ballY-y2)<=(R<<1)*(R<<1)){  // PlayeR 2 touch the ball
-        delX=(ballX-x2);
-        delY=(ballY-y2);
+        tmp=delX*(ballX-x2)+delY*(ballY-y2);
+        delX-=tmp*(ballX-x2)/R;
+        delY-=tmp*(ballY-y2)/R;
       }
       if(delX||delY){  // Ball move
         display.fillCircle(ballX,ballY,R,0);
-        lastBallX=ballX;
-        lastBallY=ballY;
         ballX+=delX;
         ballY+=delY;
         display.fillCircle(ballX,ballY,R,1);
