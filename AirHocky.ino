1affba117b680f7e134f9edc596640d615e2cedf
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-11 17:50:10 +0800

    Update AirHocky.ino

--- AirHocky.ino
+++ AirHocky.ino
@@ -7,17 +7,20 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Ball Size
-const byte VRX1=A0,VRY1=A1,VRX2=A2,VRY2=A3,R=3;
-byte score1,score2,ballX,ballY,x1,y1,x2,y2,lx1,lx2,ly1,ly2;
+const byte VRX1=A0,VRY1=A1,VRX2=A2,VRY2=A3,R=4;
+byte score1,score2,ballX,ballY,lastBallX,lastBallY,x1,y1,x2,y2,lx1,lx2,ly1,ly2;
 int delX,delY;
 void setup(){
-  Serial.begin(9600);
   pinMode(VRX1,INPUT);
   pinMode(VRY1,INPUT);
   pinMode(VRX2,INPUT);
   pinMode(VRY2,INPUT);
   pinMode(2,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(7,OUTPUT);
   digitalWrite(2,1);
+  digitalWrite(4,1);
+  digitalWrite(7,0);
   display.begin(SSD1306_SWITCHCAPVCC,0x3C);
   display.display();
   delay(1000);
@@ -28,19 +31,18 @@ void setup(){
 void loop(){
   score1=score2=0;
   ballX=SCREEN_WIDTH>>1;
+  display.clearDisplay();
   display.setCursor(0,0);
   display.print("move ahead");
   display.display();
-  delay(1000);
-  display.clearDisplay();
-  display.display();
-  //while(analogRead(VRY1)<800|| analogRead(VRY2)<800);
+  while(analogRead(VRY1)<800|| analogRead(VRY2)<800);
   while(score1<3&&score2<3){
     delX=delY=0;
     x1=lx1=SCREEN_WIDTH>>2;
     x2=lx2=(SCREEN_WIDTH>>2)+(SCREEN_WIDTH>>1);
     y1=y2=ly1=ly2=SCREEN_HEIGHT>>1;
-    ballY=SCREEN_HEIGHT>>1;
+    lastBallX=ballX;
+    ballY=lastBallY=SCREEN_HEIGHT>>1;
     display.clearDisplay();
     display.display();
     display.fillCircle(x1,y1,R,1);
@@ -79,6 +81,8 @@ void loop(){
       }
       if(delX||delY){  // Ball move
         display.fillCircle(ballX,ballY,R,0);
+        lastBallX=ballX;
+        lastBallY=ballY;
         ballX+=delX;
         ballY+=delY;
         display.fillCircle(ballX,ballY,R,1);
