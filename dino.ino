f949a61ab643f49b7f4c8d56cc133c003a58cde3
yukaichao1029 <127740692+yukaichao1029@users.noreply.github.com>
2024-01-16 12:47:52 +0800

    Update dino.ino

--- dino.ino
+++ dino.ino
@@ -6,7 +6,7 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET 4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-const byte BUTTON=2,DINO_HEIGHT=20,DINO_WIDTH=17,TREE_HEIGHT=10,TREE_WIDTH=10,PLAY_HEIGHT=10,PLAY_WIDTH=10,JUMP_MOVE=30;
+const byte JOYSTICK=A0,DINO_HEIGHT=20,DINO_WIDTH=17,TREE_HEIGHT=10,TREE_WIDTH=10,PLAY_HEIGHT=10,PLAY_WIDTH=10,JUMP_MOVE=30;
 const bool DINO[DINO_HEIGHT][DINO_WIDTH]={
   {0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0},
   {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1},
@@ -32,12 +32,12 @@ const bool DINO[DINO_HEIGHT][DINO_WIDTH]={
 bool jump,pressed;
 byte i,j,x,y,jumpMove;
 int treeX;
-void dino(){
+void dino(){//畫恐龍
   for(i=0;i<DINO_HEIGHT;i++)for(j=0;j<DINO_WIDTH;j++)display.drawPixel(j,y+i,DINO[i][j]);
 }
 void setup(){
   Serial.begin(9600);
-  pinMode(BUTTON,INPUT_PULLUP);
+  pinMode(JOYSTICK,INPUT);
   display.begin(SSD1306_SWITCHCAPVCC,0x3C);
   display.display();
   delay(1000);
@@ -56,8 +56,8 @@ void loop(){
   pressed=0;
   treeX=SCREEN_WIDTH-TREE_WIDTH;
   jumpMove=0;
-  while(digitalRead(BUTTON));
-  while(!digitalRead(BUTTON));
+  while(analogRead(JOYSTICK)>300);
+  while(analogRead(JOYSTICK)<300);
   display.fillRect(SCREEN_WIDTH-PLAY_WIDTH>>1,SCREEN_HEIGHT-PLAY_HEIGHT>>1,PLAY_WIDTH,PLAY_HEIGHT,0);
   display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT,SCREEN_WIDTH,SCREEN_HEIGHT,0);
   y=SCREEN_HEIGHT-DINO_HEIGHT;
@@ -70,7 +70,7 @@ void loop(){
       display.fillRect(treeX+10,SCREEN_HEIGHT-TREE_HEIGHT,1,TREE_HEIGHT,0);
     }
     display.display();
-    if(!digitalRead(BUTTON)&&!jump&&!pressed){
+    if(analogRead(JOYSTICK)<300&&!jump&&!pressed){
       jump=pressed=1;
       display.fillRect(0,SCREEN_HEIGHT-DINO_HEIGHT,DINO_WIDTH,DINO_HEIGHT,0);
       y=SCREEN_HEIGHT-DINO_HEIGHT-TREE_HEIGHT;
@@ -85,7 +85,7 @@ void loop(){
       dino();
       display.display();
     }
-    if(digitalRead(BUTTON))pressed=0;
+    if(analogRead(JOYSTICK)>300)pressed=0;
     display.display();
     if(treeX>=-DINO_WIDTH)treeX-=1;
     else treeX=SCREEN_WIDTH-TREE_WIDTH;
